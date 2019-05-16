8ca2e6f756642758da72a56d8cafeb0bb27c5ac4
Christian Vassallo <44880451+StoKatze@users.noreply.github.com>
2019-05-16 18:40:20 +0200

    Added GPIO.clenaup()-increased temperature trigger

--- Python/sensor.py
+++ Python/sensor.py
@@ -14,65 +14,64 @@ SENDING_PERIOD = 2
 MAX_LINES_HISTORY = 1000
 
 def main():
-
-	# DB connection params
-	dbParams = {"db": "sensori", "precision": "ms"}
-	
-	# Initialization
-	bus = smbus2.SMBus(SENSOR_PORT)
-	bme280.load_calibration_params(bus, SENSOR_ADDRESS)
-	
-	payload = ""
-	counter = 1
-	problem_counter = 0
-	
 	try:
-		GPIO.setmode(GPIO.board) # Set GPIO mode to BOARD
+		# DB connection params
+		dbParams = {"db": "sensori", "precision": "ms"}
+	
+		# Initialization
+		bus = smbus2.SMBus(SENSOR_PORT)
+		bme280.load_calibration_params(bus, SENSOR_ADDRESS)
+	
+		payload = ""
+		counter = 1
+		problem_counter = 0
+	
+		GPIO.setmode(GPIO.BOARD) # Set GPIO mode to BOARD
 		PIN_IMP = 7 # Pin 7 is connected to relay
 		GPIO.setup(PIN_IMP, GPIO.OUT) # Sets relay as output
+	
+		# Infinite loop
+		while True:
+			unix_time_ms = int(time.time()*1000)
+		
+			# Read sensor data and convert it to line protocol
+			data = bme280.sample(bus, SENSOR_ADDRESS)
+			line = "sensors_data temperature={},pressure={},humidity={} {}\n".format(data.temperature, data.pressure, data.humidity, unix_time_ms)
+		
+			payload += line
+		
+			# Verbose
+			print("Temperature: ", data.temperature)
+			print("Pressure: ", data.pressure)
+			print("Humidity: ", data.humidity)
+		
+			if data.temperature > 21:
+				GPIO.output(PIN_IMP, GPIO.HIGH) # Relay on
+			else:
+				GPIO.output(PIN_IMP, GPIO.LOW) # Realay off
+		
+			if counter % SENDING_PERIOD == 0:
+				try:
+					# Try to send data
+					r = requests.post(URL, params=dbParams, data=payload)
+					if r.status_code != 204:
+						raise Exception("data not written")
+					payload = ""
+				except:
+					problem_counter += 1
+					print('cannot write to InfluxDB')
+					if problem_counter == MAX_LINES_HISTORY:
+						problem_counter = 0
+					payload = ""
+		
+			counter += 1
+		
+			# Wait for selected time
+			time_diff_ms = int(time.time()*1000) - unix_time_ms
+			if time_diff_ms < READING_DATA_PERIOD_MS:
+				time.sleep((READING_DATA_PERIOD_MS - time_diff_ms)/1000.0)
 	finally:
 		GPIO.cleanup() # Frees GPIO pins
 		
-	# Infinite loop
-	while True:
-		unix_time_ms = int(time.time()*1000)
-		
-		# Read sensor data and convert it to line protocol
-		data = bme280.sample(bus, SENSOR_ADDRESS)
-		line = "sensors_data temperature={},pressure={},humidity={} {}\n".format(data.temperature, data.pressure, data.humidity, unix_time_ms)
-		
-		payload += line
-		
-		# Verbose
-		print("Temperature: ", data.temperature)
-		print("Pressure: ", data.pressure)
-		print("Humidity: ", data.humidity)
-		
-		if data.temperature > 21:
-			GPIO.output(PIN_IMP, GPIO.HIGH) # Relay on
-		else:
-			GPIO.output(PIN_IMP, GPIO.LOW) # Realay off
-		
-		if counter % SENDING_PERIOD == 0:
-			try:
-				# Try to send data
-				r = requests.post(URL, params=dbParams, data=payload)
-				if r.status_code != 204:
-					raise Exception("data not written")
-				payload = ""
-			except:
-				problem_counter += 1
-				print('cannot write to InfluxDB')
-				if problem_counter == MAX_LINES_HISTORY:
-					problem_counter = 0
-				payload = ""
-		
-		counter += 1
-		
-		# Wait for selected time
-		time_diff_ms = int(time.time()*1000) - unix_time_ms
-		if time_diff_ms < READING_DATA_PERIOD_MS:
-			time.sleep((READING_DATA_PERIOD_MS - time_diff_ms)/1000.0)
-			
 if __name__ == "__main__":
 	main()
